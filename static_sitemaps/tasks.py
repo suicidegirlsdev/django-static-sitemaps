866f1d5cec205cfca713ea715427e90b77ba5568
james <james@suicidegirls.com>
2019-05-07 17:53:52 -0700

    Update task for latest celery

--- static_sitemaps/tasks.py
+++ static_sitemaps/tasks.py
@@ -1,19 +1,27 @@
 from datetime import timedelta
 
-from celery.task import PeriodicTask
+from celery import Celery
+from celery import shared_task
+
+app = Celery()
 
 from static_sitemaps import conf
 from static_sitemaps.generator import SitemapGenerator
 
 __author__ = 'xaralis'
 
-# Create class conditionally so the task can be bypassed when repetition 
+# Register the task conditionally so the task can be bypassed when repetition
 # is set to something which evaluates to False.
 if conf.CELERY_TASK_REPETITION:
-    class GenerateSitemap(PeriodicTask):
-        run_every = timedelta(minutes=conf.CELERY_TASK_REPETITION)
-    
-        def run(self, **kwargs):
-            generator = SitemapGenerator(verbosity=1)
-            generator.write()
+    @app.on_after_configure.connect
+    def setup_periodic_tasks(sender, **kwargs):
+        sender.add_periodic_task(
+            timedelta(minutes=conf.CELERY_TASK_REPETITION),
+            generate_sitemap.s(),
+            expires=600, # If hasn't started in 10m, don't risk overlap
+        )
 
+@shared_task
+def generate_sitemap():
+    generator = SitemapGenerator(verbosity=1)
+    generator.write()
